b68dddcf05abd13f8f07cbb44f8825556dbb8723
Zaid Hasan Siddiqui <zaid.hasan.sddq@gmail.com>
2022-02-01 00:24:13 +0530

    final commit

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 const StreamShow = (props) => {
   useEffect(() => {
     props.fetchStream(props.match.params.id);
+    const flvPlayer = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${props.match.params.id}.flv`,
+    });
+    flvPlayer.attachMediaElement(videoRef.current);
+    flvPlayer.load();
+    return () => {
+      flvPlayer.destroy();
+    };
   }, []);
+  const videoRef = useRef(null);
   return (
     <div>
+      <video ref={videoRef} style={{ width: "100%" }} controls />
       <h1>{props.stream && props.stream.title}</h1>
       <h5>{props.stream && props.stream.description}</h5>
     </div>
